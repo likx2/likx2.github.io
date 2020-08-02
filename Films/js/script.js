f9793de15d80436fe227c32b6bffbc7b4b7a8a3a
Lev <68220709+likx2@users.noreply.github.com>
2020-08-02 23:00:19 +0300

    Finished 3 task

--- Films/js/script.js
+++ Films/js/script.js
@@ -37,9 +37,62 @@ genre.textContent = "ДРАМА";
 // // 3
 const promo__bg = document.getElementsByClassName("promo__bg")[0];
 promo__bg.style.backgroundImage = "url('../img/bg.jpg')";
-// // 4
+
+
+// New Task
 const films = document.querySelectorAll(".promo__interactive-item");
-for (let i = 0; i < films.length; i++) {
-    films[i].innerHTML = (i + 1).toString() + ". " + movieDB.movies.sort()[i];
-    films[i].insertAdjacentHTML("afterbegin", '<div class="delete"></div>');
+const btn = document.querySelector("button");
+let input = document.querySelector(".adding__input");
+const promo__list = document.querySelector(".promo__interactive-list");
+films.forEach(value => {
+    value.remove();
+})
+
+
+// Flag to delete
+let flag = false;
+
+// Add Element
+function addElement(array, target, className_1, addingTag) {
+    let list = new Array(array.length);
+
+    for (let i = 0; i < array.length; i++) {
+        list[i] = document.createElement("li");
+        if (array.sort()[i].length > 21) {
+            array.sort()[i] = array.sort()[i].slice(0, array.sort()[i].length / 2) + "...";
+        }
+        list[i].innerHTML = (i + 1).toString() + ". " + array.sort()[i];
+        list[i].classList.add(className_1);
+        target.append(list[i]);
+        list[i].insertAdjacentHTML("afterbegin", addingTag);
+    }
 }
+addElement(movieDB.movies, promo__list, "promo__interactive-item", '<div class="delete"></div>');
+
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (input.value !== "") {
+        input.style.cssText = '';
+        movieDB.movies.push(input.value);
+        const newFilms = document.querySelectorAll(".promo__interactive-item");
+        newFilms.forEach(value => {
+            value.remove();
+        })
+        addElement(movieDB.movies, promo__list, "promo__interactive-item", '<div class="delete"></div>');
+    } else {
+        input.style.cssText = 'border-color: #d63031;box-shadow: 0 0 5px #d63031;';
+    }
+
+})
+
+const bin = document.querySelectorAll('.delete');
+
+
+// Deleting alement from the doc
+bin.forEach(value => {
+    value.addEventListener("click", () => {
+        value.parentElement.remove();
+        flag = true;
+    });
+})
