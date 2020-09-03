6689ce011dc49f646ef62ed9f4a5c27d9121f6ba
Lev <68220709+likx2@users.noreply.github.com>
2020-09-03 09:55:54 +0300

    Optimized css filr

--- PowerDoc/js/script.js
+++ PowerDoc/js/script.js
@@ -16,11 +16,13 @@ const header__btn = document.querySelector(".header__btn");
 const consult__popup = document.querySelector(".consult__popup");
 const popup__content = document.querySelector(".consult__popup__content");
 header__btn.addEventListener("click", () => {
-    consult__popup.style.display = "flex";
+    showPopup(consult__popup);
 });
+
+
 const close__btn = document.querySelector(".close__btn");
 close__btn.addEventListener("click", () => {
-    consult__popup.style.display = "none";
+    hidePopup(consult__popup);
 })
 // Service Slider
 
@@ -41,3 +43,42 @@ $(document).ready(function () {
         nextArrow: '<button class="next-btn"></button>'
     });
 });
+
+// More Popup
+
+
+const more_btns = document.querySelectorAll('.more');
+const more_popups = document.querySelectorAll('.more-popup');
+more_popups.forEach(item => {
+    hidePopup(item);
+});
+
+more_btns.forEach((item, i) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        showPopup(more_popups[i]);
+        document.querySelector('body').style.overflow = 'hidden';
+        more_popups[i].style.overflow = 'scroll';
+    });
+});
+more_popups.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (event.target === item || event.target.classList.contains('close')) {
+            hidePopup(item);
+        }
+    })
+})
+
+
+
+
+function showPopup(object) {
+    object.style.display = "flex";
+
+}
+
+function hidePopup(object) {
+    object.style.display = "none";
+
+}
