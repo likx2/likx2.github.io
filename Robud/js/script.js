6d78414251fab6a756c38c7b7d7c35655bba4fc4
Lev <68220709+likx2@users.noreply.github.com>
2020-10-17 22:40:04 +0200

    Added smooth scroll

--- Robud/js/script.js
+++ Robud/js/script.js
@@ -1,11 +1,39 @@
-const nav = document.querySelector('.header__nav'),
-    burgerBtn = document.querySelector('.header__burger'),
-    burgerLines = document.querySelectorAll('.line');
+    document.addEventListener('DOMContentLoaded', () => {
+        const nav = document.querySelector('.header__nav'),
+            burgerBtn = document.querySelector('.header__burger'),
+            burgerLines = document.querySelectorAll('.line');
 
 
-burgerBtn.addEventListener('click', () => {
-    nav.classList.toggle('header__nav_active');
-    burgerLines.forEach((item, index) => {
-        item.classList.toggle(`toggle-line-${index+1}`);
-    })
-});
+        burgerBtn.addEventListener('click', () => {
+            nav.classList.toggle('header__nav_active');
+            burgerLines.forEach((item, index) => {
+                item.classList.toggle(`toggle-line-${index+1}`);
+            })
+        });
+
+        function scrollToSection(target) {
+            let targetElement = document.querySelector(target);
+            let targetYOffset = targetElement.offsetTop;
+            window.scrollTo({
+                top: targetYOffset,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }
+
+        const navItems = document.querySelectorAll('.header__nav-item'),
+            mainBtns = document.querySelectorAll('.main__btn');
+
+        navItems.forEach(item => {
+            item.addEventListener('click', () => {
+                scrollToSection(item.getAttribute('data-scroll'));
+            });
+        });
+        mainBtns.forEach(item => {
+            item.addEventListener('click', () => {
+                scrollToSection(item.getAttribute('data-scroll'));
+            });
+        });
+
+
+    });
