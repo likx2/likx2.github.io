ebf22e96698b2c601a0dce2fcc85b18bdb75d13a
Lev <konopajew86@gmail.com>
2021-01-16 22:33:09 +0100

    fix bugs

--- Robud/js/script.js
+++ Robud/js/script.js
@@ -3,11 +3,7 @@
             burgerBtn = document.querySelector('.header__burger'),
             burgerLines = document.querySelectorAll('.line'),
             navItems = document.querySelectorAll('.header__nav-item'),
-            mainBtns = document.querySelectorAll('.main__btn'),
-            termsBtn = document.querySelector('.footer__terms'),
-            termsModal = document.querySelector('.terms-modal'),
-            closeBtn = document.querySelector('.terms-modal__close')
-
+            mainBtns = document.querySelectorAll('.main__btn')
 
         burgerBtn.addEventListener('click', () => {
             nav.classList.toggle('header__nav_active');
@@ -19,13 +15,12 @@
         function scrollToSection(target) {
             let targetElement = document.querySelector(target);
             let targetYOffset = targetElement.offsetTop;
-            window.scrollTo({
+            window.scroll({
                 top: targetYOffset,
                 left: 0,
                 behavior: 'smooth'
             });
         }
-
         navItems.forEach(item => {
             item.addEventListener('click', () => {
                 scrollToSection(item.getAttribute('data-scroll'));
@@ -36,15 +31,4 @@
                 scrollToSection(item.getAttribute('data-scroll'));
             });
         });
-        // PDF
-        PDFObject.embed('/pdf/terms_and_conditions.pdf', '#pdf')
-
-        termsBtn.addEventListener('click', () => {
-            termsModal.classList.toggle('terms-modal_active')
-            document.body.style.overflow = 'hidden'
-        })
-        closeBtn.addEventListener('click', () => {
-            termsModal.classList.toggle('terms-modal_active')
-            document.body.style.overflow = ''
-        })
     });
